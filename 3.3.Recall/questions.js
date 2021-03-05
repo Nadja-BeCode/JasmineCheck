f883f32b82970705cba8733a3a4798796f2aea45
Nadja-BeCode <nadja.agostinho@gmail.com>
2021-03-05 14:44:00 +0100

    added others and modif

--- 3.3.Recall/questions.js
+++ 3.3.Recall/questions.js
@@ -52,9 +52,10 @@ let addElementToBeginning = (array, element) => {
 }
 
 let sortByLastLetter = (array) => {
-   /* return array.sort(
+        return array.sort(
         (a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1)
-    );*/
+    );//A RETRAVAILLER
+
 }
 
 let getFirstHalf = (string) => {
@@ -77,11 +78,25 @@ let numberOfPalindromes = (array) => {
 }
 
 let shortestWord = (array) => {
-    return ;
+   let short = array.reduce((a, b) => {
+        return a.length <= b.length ? a : b;
+    })
+    return short;
 }
 
+//return array.reduce((a, b) => a.length <= b.length ? a : b)
+//tester avec reduce , filtre, sort (OU) :
+
+/* let arr = [];
+    let string = 'ttttttttttttttttttttttttttttttttttttttttttttttttttt';
+    for (let i = 0; i < array.length; i++) {
+        if(string.length > array[i].length){
+
+        }
+    }*/
+
 let longestWord = (array) => {
-    return ;
+    return array.reduce((a, b) => a.length >= b.length ? a : b);
 }
 
 let sumNumbers = (array) => {
@@ -118,58 +133,122 @@ let getElementsUntilGreaterThanFive = (array) => {
 }
 
 let convertArrayToObject = (array) => {
-    return 'Write your method here';
+    
+    let arr =[];
+    for (let i = 0; i < array.length; i+=2) {
+        arr.push([array[i], array[i+1]])
+    }
+    let obj = Object.fromEntries(arr);
+    return obj
+    
+    /*let newArr = [];
+    array.forEach(obj => {
+        for(let i=0; array.length; i++);
+    }); obj[i] = newArr[i];
+    return newArr;*/
 }
 
 let getAllLetters = (array) => {
-    return 'Write your method here';
+    //It transforms array in string
+    let string = array.join("");
+    console.log(string);
+    //It transforms the string in an array with letters
+    let arrayOfLetters = string.split("").map(x => x).sort();
+    console.log(arrayOfLetters);
+    //It deletes doubles of letters
+    let deleteDouble = [...new Set(arrayOfLetters)];
+    console.log(deleteDouble)
+    return deleteDouble;
+
+    //tableau de mots , prendre tout ces mots en string
+    //split le string
+    //mettre en tableau
+    //trier le tablaeu
+    //retier element repeter
+    
+    /*let newArr = [];
+    array.forEach( words => {
+        array.split('').push
+    });
+    return newArr;*/
 }
 
 let swapKeysAndValues = (object) => {
-    return 'Write your method here';
+    //permuter des valeurs dans un tableau + parcourir les elements d'un object avec le forEach
+    return ;
 }
 
 let sumKeysAndValues = (object) => {
-    return 'Write your method here';
+    //calculer somme 
+    //recuper val d'un obj
+    //puis les additionnier
+    //2eme variable pour recuperer les keys des objects
+    //convertir en entier puis faire la somme
+    return;
 }
 
 let removeCapitals = (string) => {
-    string = string.slice(1);
-    return string;
+    let newString = string.replace(/[A-Z]/g, '');
+    return newString
+
+    /*string = string.slice(1);
+    return string; = ATTENTION TESTER AVEC Le SLICE*/
 }
 
 let roundUp = (number) => {
-    Math.round()
-    return Math.round(number);
+    return Math.ceil(number);
 }
 
 let formatDateNicely = (date) => {
-    return 'Write your method here';
+    let europeanDate = date.toLocaleString("fr-Fr", {
+        month: "numeric",
+        day: "numeric",
+        year: "numeric",
+    })
+    
+    return europeanDate;
+
+    /*let event = new Date(date.UTC('2015-03-04');
+    return (event.toLocaleString('fr-FR'));*/
+
 }
 
 let getDomainName = (string) => {
     return 'Write your method here';
-}
+    /*Let a = "manger"
+
+    afficher juste le "ger" de manger 
+
+    a.replace("man" , "")
+
+    1) premier parametre la partie a remplacer
+    2) ce que je vais mettre a la placer de man*/
+    
+}//regex & replace
 
 let titleize = (string) => {
     return 'Write your method here';
 }
 
 let checkForSpecialCharacters = (string) => {
-    return 'Write your method here';
+    let specialCharact = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    return specialCharact.test(string);
 }
 
 let squareRoot = (number) => {
-    
-    return Math.sqrt(9);
-    return Math.sqrt(9)
-    
+    return Math.sqrt(number);
+     
 }
 
 let factorial = (number) => {
-    if (number < 0) return -1
-    if (number === 0) return 1;
-    return number * factorial(number - 1);;
+    return (number != 1) ? number * factorial(number - 1) : 1;
+    
+    //return number * factorial(number - 1);;
+    /*let f = 1;
+    for (let i = 1; i <= number; i++) {
+        f = f * i
+    }
+    return f;*/
 }
 
 let findAnagrams = (string) => {
@@ -177,10 +256,12 @@ let findAnagrams = (string) => {
 }
 
 let convertToCelsius = (number) => {
-    let Fahrenheit = Fahrenheit => (Fahrenheit - 32) * 5/9;
-    return Fahrenheit;
+    return Math.round((number - 32) * 5 / 9);
+
+    //let celsiusToFahrenheit = number => number * 9/5 + 32;
+    //let celsius = number => (number - 32) * 5/9;
 }
 
 let letterPosition = (array) => {
-    return 'Write your method here';
+    return array.map((e) => e.toUpperCase().charCodeAt(0) - 64);
 }
